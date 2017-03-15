d2feccb4de866a098e5bdf05215c0cffa7d172c0
DragonXz <omarkwedar@gmail.com>
2017-03-15 03:58:37 +0200

    Update Establishing-a-Build-Environment.sh

--- Establishing-a-Build-Environment.sh
+++ Establishing-a-Build-Environment.sh
@@ -1,27 +1,3 @@
-# Created By Hamada Jimmy "GMCadimo" <gm.cadiom@gmail.com>
-# CopyRights To Me And My WebSite www.docroid.tk
-clear
 echo Installing Dependencies!
-sudo apt-get update
-sudo apt-get -y install git-core python gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev \
-squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-7-jdk pngcrush \
-schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev \
-gcc-multilib liblz4-* pngquant ncurses-dev texinfo gcc gperf patch libtool \
-automake g++ gawk subversion expat libexpat1-dev python-all-dev binutils-static bc libcloog-isl-dev \
-libcap-dev autoconf libgmp-dev build-essential gcc-multilib g++-multilib pkg-config libmpc-dev libmpfr-dev lzma* \
-liblzma* w3m phablet-tools android-tools-adb ccache maven maven2 ncftp
-echo Dependencies have been installed
-echo Downloading Repo!
-mkdir -p ~/bin && curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
-echo Repo Has Been Downloaded!
-if [ ! "$(which adb)" == "" ];
-then
-echo Setting up USB Ports
-sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/GMCadiom/MY-SCRIPT/master/51-android.rules
-sudo chmod 644   /etc/udev/rules.d/51-android.rules
-sudo chown root /etc/udev/rules.d/51-android.rules
-sudo service udev restart
-adb kill-server
-sudo killall adb
-echo USB Ports Configured!
+cd bionic && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_bionic cm-13.0 && cd .. && cd build && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_build cm-13.0 && cd .. && cd external/tinyalsa && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_external_tinyalsa cm-13.0 && cd ../.. && cd external/tinycompress && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_external_tinycompress cm-13.0 && cd ../.. && cd external/sepolicy && git pull https://github.com/Lineage-OS-13-MT6592/android_external_sepolicy cm-13.0 && cd ../.. && cd external/wpa_supplicant_8 && git pull https://github.com/Lineage-OS-13-MT6592/android_external_wpa_supplicant_8 cm-13.0 && cd ../.. && cd frameworks/av && git pull https://github.com/Lineage-OS-13-MT6592/android_frameworks_av cm-13.0 && cd ../.. && cd frameworks/base && git pull https://github.com/Lineage-OS-13-MT6592/android_frameworks_base cm-13.0 && cd ../.. && cd frameworks/native && git pull https://github.com/Lineage-OS-13-MT6592/android_frameworks_native cm-13.0 && cd ../.. && cd frameworks/opt/net/ims && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_frameworks_opt_net_ims cm-13.0 && cd ../../../.. && cd frameworks/opt/telephony && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_frameworks_opt_telephony cm-13.0 && cd ../../.. && cd frameworks/rs && git pull https://github.com/Lineage-OS-13-MT6592/android_frameworks_rs cm-13.0 && cd ../.. && cd hardware/libhardware && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_hardware_libhardware cm-13.0 && cd ../.. && cd hardware/libhardware_legacy && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_hardware_libhardware_legacy cm-13.0 && cd ../.. && cd packages/services/Telecomm && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_packages_services_Telecomm cm-13.0 && cd ../../.. && cd packages/services/Telephony && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_packages_services_Telephony cm-13.0 && cd ../../.. && cd packages/apps/Screencast && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_packages_apps_Screencast cm-13.0 && cd ../../.. && cd packages/apps/Snap && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_packages_apps_Snap cm-13.0 && cd ../../.. && cd packages/apps/Settings && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_packages_apps_Settings cm-13.0 && cd ../../.. && cd system/bt && git pull https://github.com/Lineage-OS-13-MT6592/android_system_bt cm-13.0 && cd ../.. && cd system/core && git pull https://github.com/Lineage-OS-13-MT6592/android_system_core cm-13.0 && cd ../.. && cd system/media && git pull https://github.com/MTK-mt65xx-CM-Project-MM/android_system_media cm-13.0 && cd ../.. && cd system/netd && git pull https://github.com/Lineage-OS-13-MT6592/android_system_netd cm-13.0 && cd ../.. && cd system/vold && git pull https://github.com/Lineage-OS-13-MT6592/android_system_vold cm-13.0 && cd ../..
 fi
